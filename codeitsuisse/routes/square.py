fe77f919d0d30d6368d6f4cb51b9b29afb4f5be5
Theo Azriel <theoazrie@gmail.com>
2020-09-25 18:19:38 +0700

    Update square.py

--- codeitsuisse/routes/square.py
+++ codeitsuisse/routes/square.py
@@ -9,12 +9,13 @@ logger = logging.getLogger(__name__)
 
 @app.route('/square', methods=['POST'])
 def evaluate():
-    data = request.get_json();
-    logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input");
-    result = inputValue * inputValue
-    logging.info("My result :{}".format(result))
-    return json.dumps(result);
+    return "HELLO"
+    # data = request.get_json();
+    # logging.info("data sent for evaluation {}".format(data))
+    # inputValue = data.get("input");
+    # result = inputValue * inputValue
+    # logging.info("My result :{}".format(result))
+    # return json.dumps(result);
 
 
 
