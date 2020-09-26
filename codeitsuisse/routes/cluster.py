0faa513c2a9f306e79365d954b6e82cd769312a9
Theo Azriel <theoazrie@gmail.com>
2020-09-26 17:46:05 +0700

    Update cluster.py

--- codeitsuisse/routes/cluster.py
+++ codeitsuisse/routes/cluster.py
@@ -18,15 +18,14 @@ def findCluster():
     grid = data
 
     def dfs(grid, i, j):
-        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] == '*':
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] == '*' or grid[i][j] == '1':
             return
-        if (grid[i][j] == '0'):
-            grid[i][j] = '1'
-
-            dfs(grid, i+1, j)
-            dfs(grid, i-1, j)
-            dfs(grid, i, j+1)
-            dfs(grid, i, j-1)
+    
+        grid[i][j] = '1'
+        dfs(grid, i+1, j)
+        dfs(grid, i-1, j)
+        dfs(grid, i, j+1)
+        dfs(grid, i, j-1)
 
     if not grid:
         return json.dumps({ "answer" : 0 })
