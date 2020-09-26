62653bb9339d1873fd5541a46d0c617a342d14ce
Theo Azriel <theoazrie@gmail.com>
2020-09-26 08:55:52 +0700

    Add /salad-spree endpoint

--- codeitsuisse/routes/square.py
+++ codeitsuisse/routes/square.py
@@ -9,13 +9,29 @@ logger = logging.getLogger(__name__)
 
 @app.route('/square', methods=['POST'])
 def evaluate():
-    
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
     inputValue = data.get("input");
     result = inputValue * inputValue
     logging.info("My result :{}".format(result))
     return json.dumps(result);
+
+@app.route('/salad-spree', methods=['POST'])
+def checkSalad():
+    data = request.get_json();
+    logging.info("data received: {}".format(data))
+
+    # Parse data
+    n = data.get("number_of_salads");
+    s = data.get("salad_prices_street_map")
+
+    # result
+    sol = {
+        "n" : n,
+        "s" : s
+    }
+
+    return json.dumps(sol);
 
 
 
